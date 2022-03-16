a021a74112b5932551c25697ee95356c77c4b9b5
Sarvar8950 <sarvar8950@gmail.com>
2022-03-17 01:53:30 +0530

    use json-server

--- src/Components/Grocery.jsx
+++ src/Components/Grocery.jsx
@@ -5,26 +5,51 @@ import {v4 as uuid} from "uuid"
 
 function Grocery() {
     const [item, setItem] = React.useState([])
+    function getdata() {
+        fetch(`http://localhost:3001/todos`)
+        .then((res) => res.json())
+        .then((res) => setItem(res))
+    }
+
+    React.useEffect(() => {
+        getdata()
+    }, [])
+
     function addList(inp) {
         var obj = {
             title:inp,
             status:false,
-            id : uuid()
+            uuid_id : uuid()
         }
         setItem([...item, obj])
+
+        const jsonobj = JSON.stringify(obj)
+        fetch(`http://localhost:3001/todos`, {
+            method : "POST",
+            body : jsonobj,
+            headers : {
+                "content-type" : "application/json"
+            }
+        })
     }
+
     function togglebtn(id) {
         var updata = item.map((item) => 
             item.id === id ? {...item,status : !item.status} : item 
         )
         setItem(updata)
     }
+
     function deletebtn(id) {
         var updata = item.filter((item) => 
             item.id !== id && item 
         )
         setItem(updata)
+        fetch(`http://localhost:3001/todos/${id}`,{
+            method : "DELETE"
+        })
     }
+
     return (
         <>
             <GroceryInput addList={addList}/>
